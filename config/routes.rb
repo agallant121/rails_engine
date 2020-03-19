be34ce481d0be07b94acc64127df81966fafd0ab
Alex Gallant <agallant1201@gmail.com>
2020-03-19 13:45:00 -0600

    Add resource for merchants

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :items, only: [:index, :show, :create, :update]
+      resources :items, only: [:index, :show, :create, :update, :destroy]
+      resources :merchants, only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
