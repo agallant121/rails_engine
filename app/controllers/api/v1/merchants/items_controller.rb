08dbd5199d5d871962f38e3389ec7230124599df
Alex Gallant <agallant1201@gmail.com>
2020-03-20 16:02:06 -0600

    Add nested controller to get to a merchant's items index

---
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::Merchants::ItemsController < ApplicationController
+  def index
+    # require "pry"; binding.pry
+    render json: ItemSerializer.new(Merchant.find(params[:merchant_id]).items)
+  end
+end
