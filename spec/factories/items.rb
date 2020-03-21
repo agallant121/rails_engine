2c4c58fa5eff03486c5ea652d76d7e2b19495c07
Alex Gallant <agallant1201@gmail.com>
2020-03-21 10:57:21 -0600

    Add updated and created at attributes for the item factory

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,6 +3,8 @@ FactoryBot.define do
     name { "Banana Stand" }
     description { "There's always money in the banana stand." }
     unit_price { 125.55 }
+    # created_at { "2012-03-27 14:53:59 UTC" }
+    # updated_at { "2013-03-27 14:53:59 UTC" }
     merchant
   end
 end
