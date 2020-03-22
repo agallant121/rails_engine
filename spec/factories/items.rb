0fd425b362aac6e4fb876d6d20a4cf7b6183d9de
Alex Gallant <agallant1201@gmail.com>
2020-03-22 11:42:28 -0600

    Add fakers to factories

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :item do
-    name { Faker::Name.name }
-    description { Faker::Movies::PrincessBride.quote }
+    name { Faker::TvShows::BreakingBad.character }
+    description { Faker::TvShows::DumbAndDumber.quote}
     unit_price { rand(1.00..99.99).round(2) }
     merchant
   end
